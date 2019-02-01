f93849882d6729677a682fd5808893c04ae54628
Okigbo <okigbookey@gmail.com>
2019-02-01 15:19:36 -0500

    Update

--- basis101.py
+++ basis101.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
+import os
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import datetime
 import pandas_datareader as pdr
 
+path = "C:\\Users\\okigboo\\Desktop\\TimeSeriesAnalysis"
+os.chdir(path)
 
 symbol = 'AAPL'
 starttime = datetime.datetime(2010, 1, 1)
@@ -22,6 +25,10 @@ print(apple.head())
 print(dowJones.head())
 print(usBond.head())
 
+
+amazon = pd.read_csv('AMZN.csv', parse_dates=True, index_col='Date')
+print(amazon.head())
+print(len(amazon))
 
 # # This formats the plots such that they appear on separate rows
 # fig, axes = plt.subplots(nrows=2, ncols=1)
