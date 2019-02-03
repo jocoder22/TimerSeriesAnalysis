cb941567ba79dbb50ea2b470b3f0417288056804
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-02-03 15:09:21 -0500

    Update

--- randomNoise.py
+++ randomNoise.py
@@ -9,6 +9,16 @@ import pandas_datareader as pdr
 
 from statsmodels.tsa.stattools import acf
 from statsmodels.graphics.tsaplots import plot_acf
+from statsmodels.tsa.stattools import adfuller
+
+path = 'C:\\Users\\Jose\\Desktop\\TimerSeriesAnalysis'
+os.chdir(path)
+
+starttime = datetime.datetime(1997, 5, 15)
+endtime = datetime.datetime(2017, 8, 2)
+
+symbol = 'AMZN'
+AMZN = pdr.get_data_yahoo(symbol, starttime, endtime)
 
 
 ################## White Noise
@@ -30,7 +40,6 @@ plot_acf(returns, lags=20)
 plt.show()
 
 
-
 ############### Simulate stock prices
 # Generate 500 random steps with mean=0 and standard deviation=1
 steps = np.random.normal(loc=0, scale=1, size=500)
@@ -45,3 +54,27 @@ P = 100 + np.cumsum(steps)
 plt.plot(P)
 plt.title("Simulated Random Walk")
 plt.show()
+
+
+# Generate 500 random steps
+steps = np.random.normal(loc=0.001, scale=0.01, size=500) + 1
+
+# Set first element to 1
+steps[0]=1
+
+# Simulate the stock price, P, by taking the cumulative product
+P = 100 * np.cumprod(steps)
+
+# Plot the simulated stock prices
+plt.plot(P)
+plt.title("Simulated Random Walk with Drift")
+plt.show()
+
+
+
+# Run the ADF test on the price series and print out the results
+results = adfuller(AMZN['Adj Close'])
+print(results)
+
+# Just print out the p-value
+print('The p-value of the test on prices is: ' + str(results[1]))
