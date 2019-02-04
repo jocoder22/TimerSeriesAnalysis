3c59b644409b640ca85ef8fdc842a91884371783
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-02-04 04:09:32 -0500

    Update

--- Regression/A_regression.py
+++ Regression/A_regression.py
@@ -5,6 +5,7 @@ from sklearn import datasets
 plt.style.use('ggplot')
 
 from statsmodels.tsa.arima_process import ArmaProcess
+from statsmodels.graphics.tsaplots import plot_acf
 
 # Plot 1: AR parameter = +0.9
 plt.subplot(3,1,1)
@@ -26,7 +27,7 @@ plt.plot(simulated_data_2)
 plt.title("AR parameter = +0.3")
 plt.xticks([])
 
-# Plot 2: AR parameter = -0.9
+# Plot 3: AR parameter = -0.9
 plt.subplot(3,1,3)
 ar3 = np.array([1, 0.9])
 ma3 = np.array([1])
@@ -37,7 +38,24 @@ plt.title("AR parameter = -0.9")
 plt.show()
 
 
-
+####### Plotting ACF
 fig, (ax1, ax2, ax3) = plt.subplots(nrows=3)
-ax1.xaxis.tick_top()
+# ax1.xaxis.tick_top()
 ax2.axes.get_xaxis().set_visible(False)
+ax1.axes.get_xaxis().set_visible(False)
+
+# Plot 1: AR parameter = +0.9
+plot_acf(simulated_data_1, alpha=1, lags=20, ax=ax1)
+ax1.set_title('AR parameter = +0.9')
+# plt.show()
+
+# Plot 2: AR parameter = +0.3
+plot_acf(simulated_data_2, alpha=1, lags=20, ax=ax2)
+ax2.set_title('AR parameter = +0.3')
+# plt.show()
+
+# Plot 3: AR parameter = -0.9
+plot_acf(simulated_data_3, alpha=1, lags=20, ax=ax3)
+ax3.set_title('AR parameter = -0.9')
+plt.show()
+
