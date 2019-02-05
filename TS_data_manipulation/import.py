d17f9c79834ce6c812e9caaf88a99b14aaf47814
Okigbo <okigbookey@gmail.com>
2019-02-05 18:14:50 -0500

    Update

--- TS_data_manipulation/import.py
+++ TS_data_manipulation/import.py
@@ -7,7 +7,7 @@ import pandas_datareader as pdr
 
 ticker = ['RIO', 'ILMN', 'CPRT', 'EL', 'AMZN', 'PAA', 'GS', 'AMGN', 'MA', 'TEF', 'AAPL', 'UPS']
 
-starttime = datetime.datetime(2000, 1, 1)
+starttime = datetime.datetime(2010, 1, 4)
 endtime = datetime.datetime(2019, 1, 31)
 stocks = pdr.get_data_yahoo(ticker, starttime, endtime)['Adj Close']
 stocks.fillna(method='backfill', inplace=True)
@@ -18,3 +18,18 @@ price_return = stocks.iloc[-1].div(stocks.iloc[0]).sub(1).mul(100)
 # Plot horizontal bar chart of sorted price_return   
 price_return.sort_values().plot(kind='barh', title='Stock Price Returns')
 plt.show()
+
+
+from datetime import datetime
+import pandas_datareader.data as wb
+
+ticker2 = ['AMD','GOOG','NYT', 'FB', 'CNF', 'AMZN','BABA']
+
+start = datetime(2000,1,1)
+end = datetime(2018,12,31)
+
+stock2 = wb.DataReader(ticker2, 'yahoo',start,end)
+print(type(stock2))
+print(stock2.axes)
+print(stock2.keys())
+print(stock2['Adj Close'])
