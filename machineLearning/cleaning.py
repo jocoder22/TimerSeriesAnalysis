9f1fdb8460834ed23ec2a732e278659042b6859f
Okigbo <okigbookey@gmail.com>
2019-02-07 17:12:24 -0500

    Update

--- machineLearning/cleaning.py
+++ machineLearning/cleaning.py
@@ -26,15 +26,41 @@ plt.show()
 missing_values = close_price.isna().sum()
 print(missing_values)
 
+stocks = ['BAC']
+cprice = pdr.get_data_yahoo(stocks, startdate, enddate)['Adj Close']
+
+cprice[24:90] = np.nan
+cprice[524:570] = np.nan
+cprice[894:956] = np.nan
 # linear interpolations
-close_price_pol = close_price.interpolate('linear')
+close_price_pol = cprice.interpolate('linear')
 
 
 # plot the interoplated values
 fig, ax = plt.subplots()
-ax = close_price_pol.plot(c='g')
-close_price.plot(c='k', ax=ax, lw=3)
+close_price_pol.plot(c='g', ax=ax, lw=2)
+cprice.plot(c='k', ax=ax)
 plt.show()
 
 
+# Create a function we'll use to interpolate and plot
+def interpolate_and_plot(prices, interpolation):
+
+    # Create a boolean mask for missing values
+    missing_values = prices.isna()
+
+    # Interpolate the missing values
+    prices_interp = prices.interpolate(interpolation)
+
+    # Plot the results, highlighting the interpolated values in black
+    fig, ax = plt.subplots(figsize=(10, 5))
+    prices_interp.plot(color='k', alpha=.6, ax=ax, legend=False)
+
+    # Now plot the interpolated values on top in red
+    prices_interp[missing_values].plot(ax=ax, color='r', lw=3, legend=False)
+    plt.show()
+
+
+interpolation_type = 'zero'
+interpolate_and_plot(cprice, interpolation_type)
 
