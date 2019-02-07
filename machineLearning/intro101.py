9a3b5eabe442862bd345151d78657aaab25d426d
Okigbo <okigbookey@gmail.com>
2019-02-06 19:23:57 -0500

    Update

--- machineLearning/intro101.py
+++ machineLearning/intro101.py
@@ -110,34 +110,34 @@ plt.show()
 # plt.show()
 
 
-# Rectify the audio signal
+# # Rectify the audio signal
 audio = pd.Series(audio)
-audio_rectified = audio.apply(np.abs)
+# audio_rectified = audio.apply(np.abs)
 
-# Plot the result
-audio_rectified.plot()
-plt.show()
-
-
-# Smooth by applying a rolling mean
-roll = int(audio.shape[0] / sfreq  * 50)
-audio_rectified_smooth = audio_rectified.rolling(roll).mean()
-
-# Plot the result
-audio_rectified_smooth.plot()
-plt.show()
+# # Plot the result
+# audio_rectified.plot()
+# plt.show()
 
 
-# Calculate stats
-means = np.mean(audio_rectified_smooth, axis=0)
-stds = np.std(audio_rectified_smooth, axis=0)
-maxs = np.max(audio_rectified_smooth, axis=0)
+# # Smooth by applying a rolling mean
+# roll = int(audio.shape[0] / sfreq  * 50)
+# audio_rectified_smooth = audio_rectified.rolling(roll).mean()
 
-# Create the X and y arrays
-label = 'nomal'
-labels = np.array(label)
-X = np.column_stack([means, stds, maxs])
-y = labels.reshape([-1, 1])
+# # Plot the result
+# audio_rectified_smooth.plot()
+# plt.show()
+
+
+# # Calculate stats
+# means = np.mean(audio_rectified_smooth, axis=0)
+# stds = np.std(audio_rectified_smooth, axis=0)
+# maxs = np.max(audio_rectified_smooth, axis=0)
+
+# # Create the X and y arrays
+# label = 'nomal'
+# labels = np.array(label)
+# X = np.column_stack([means, stds, maxs])
+# y = labels.reshape([-1, 1])
 
 # Fit the model and score on testing data
 # percent_score = cross_val_score(model, X, y, cv=5)
@@ -167,12 +167,36 @@ print(audio[:10])
 HOP_LENGTH = 2**4
 spec = stft(audio, hop_length=HOP_LENGTH, n_fft=2**7)
 
-
+print(type(spec))
+print(spec)
+print(spec.shape)
 # Convert into decibels
+spec_db = amplitude_to_db(np.abs(spec))
+
+# # Compare the raw audio to the spectrogram of the audio
+# fig, axs = plt.subplots(2, 1, figsize=(10, 10), sharex=True)
+# axs[0].plot(time, audio)
+# specshow(spec_db, sr=sfreq, x_axis='time', y_axis='hz', hop_length=HOP_LENGTH)
+# plt.show()
+
+
+# Calculate the spectral centroid and bandwidth for the spectrogram
+# bandwidths = lr.feature.spectral_bandwidth(S=spec)[0]
+# centroids = lr.feature.spectral_centroid(S=spec)[0]
+
+bandwidths = lr.feature.spectral_bandwidth(S=spec)[0]
+centroids = lr.feature.spectral_centroid(S=spec)[0]
+
+
+# Convert spectrogram to decibels for visualization
 spec_db = amplitude_to_db(spec)
 
-# Compare the raw audio to the spectrogram of the audio
-fig, axs = plt.subplots(2, 1, figsize=(10, 10), sharex=True)
-axs[0].plot(time, audio)
-specshow(spec_db, sr=sfreq, x_axis='time', y_axis='hz', hop_length=HOP_LENGTH)
+# Display these features on top of the spectrogram
+fig, ax = plt.subplots(figsize=(10, 5))
+ax = specshow(spec_db, x_axis='time', y_axis='hz', hop_length=HOP_LENGTH)
+ax.plot(times_spec, centroids)
+ax.fill_between(times_spec, centroids - bandwidths / 2,
+                centroids + bandwidths / 2, alpha=.5)
+ax.set(ylim=[None, 6000])
+plt.show()
 
