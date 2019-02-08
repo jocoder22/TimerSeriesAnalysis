519093df8cfa9276d25d0927893509b521cc01b1
Okigbo <okigbookey@gmail.com>
2019-02-08 11:38:26 -0500

    Update

--- machineLearning/timeShifted.py
+++ machineLearning/timeShifted.py
@@ -13,6 +13,23 @@ from statsmodels.graphics.tsaplots import plot_pacf
 startdate = datetime(2010, 1, 4)
 enddate = datetime(2015, 1, 31)
 
-stock = pdr.get_data_yahoo('APPL', startdate, enddate)['Adj Close']
+stock = pdr.get_data_yahoo('AAPL', startdate, enddate)['Adj Close']
 
 print(stock.head())
+
+
+# These are the "time lags"
+shifts = np.arange(1, 8).astype(int)
+
+# Use a dictionary comprehension to create name: value pairs, one pair per shift
+shifted_stock = {"lag_{}_day".format(shifted): stock.shift(
+    shifted) for shifted in shifts}
+
+# Convert into a DataFrame for subsequent use
+stock_shifted = pd.DataFrame(shifted_stock)
+
+# Plot the first 100 samples of each
+ax = stock_shifted.iloc[:100].plot(cmap=plt.cm.viridis)
+stock.iloc[:100].plot(color='r', lw=2)
+ax.legend(loc='best')
+plt.show()
