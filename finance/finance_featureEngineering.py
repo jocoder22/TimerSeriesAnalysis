7937b83fa915af8865888742de5f463d1d2199dd
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-02-09 16:39:23 -0500

    Update

--- finance/finance_featureEngineering.py
+++ finance/finance_featureEngineering.py
@@ -5,6 +5,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import pandas_datareader as pdr
 from datetime import datetime
+import seaborn as sns
 from statsmodels.tsa.arima_process import ArmaProcess
 from statsmodels.graphics.tsaplots import plot_acf
 from statsmodels.tsa.arima_model import ARMA
@@ -31,6 +32,12 @@ lng_df.columns = ['Adj_Close', 'Volume']
 
 
 
+lng_df['5d_close_future_pct'] = lng_df['Adj_Close'].shift(-5).pct_change(5)
+lng_df['5d_close_pct'] = lng_df['Adj_Close'].pct_change(5)
+
+spy_df['5d_future_close'] = spy_df['Adj_Close'].shift(-5)
+spy_df['5d_close_future_pct'] = spy_df['5d_future_close'].pct_change(5)
+spy_df['5d_close_pct'] = spy_df['Adj_Close'].pct_change(5)
 
 
 
@@ -82,5 +89,71 @@ targets = lng_df['5d_close_future_pct']
 targets2 = spy_df['5d_close_future_pct']
 
 # Create DataFrame from target column and feature columns
-feat_targ_df = lng_df[['5d_close_future_pct'] + feature_names]# Drop all na values
+feat_targ_df = lng_df[['5d_close_future_pct'] + feature_names]
 
+# Calculate correlation matrix
+corrm = feat_targ_df.corr()
+print(corrm)
+
+#  Plot heatmap of correlation matrix
+sns.heatmap(corrm, annot=True)
+plt.yticks(rotation=0); plt.xticks(rotation=90)  # fix ticklabel directions
+plt.tight_layout()  # fits plot area to the plot, "tightly"
+plt.show()  # show the plot
+plt.clf()  # clear the plot area
+
+columnnames = list(corrm.columns.values)
+# Create a scatter plot of the most highly correlated variable with the target
+plt.scatter(np.arange(len(corrm)), corrm['5d_close_future_pct'])
+for idx, (val, vname) in enumerate(zip(corrm['5d_close_future_pct'], columnnames)):
+    offset = 0.05
+    plt.text(idx+offset, val + offset, vname, ha='center', va='center')
+plt.show()
+
+print(list(corrm.columns.values))
+
+
+
+plt.scatter(lng_df['ma200'], lng_df['5d_close_future_pct'])
+plt.show()
+
+
+
+# Import the statsmodels.api library with the alias sm
+import statsmodels.api as sm
+
+# Add a constant to the features
+linear_features = sm.add_constant(features)
+
+# Create a size for the training set that is 85% of the total number of samples
+train_size = int(0.85 * features.shape[0])
+train_features = linear_features[:train_size]
+train_targets = targets[:train_size]
+test_features = linear_features[train_size:]
+test_targets = targets[train_size:]
+print(linear_features.shape, train_features.shape, test_features.shape)
+
+
+# Create the linear model and complete the least squares fit
+model = sm.OLS(train_targets, train_features)
+results = model.fit()  # fit the model
+print(results.summary())
+
+# examine pvalues
+# Features with p <= 0.05 are typically considered significantly different from 0
+print(results.pvalues)
+
+
+# Scatter the predictions vs the targets with 80% transparency
+plt.scatter(train_predictions, train_targets, alpha=0.2, color='b', label='train')
+plt.scatter(test_predictions, test_targets, alpha=0.2, color='r', label='test')
+
+# Plot the perfect prediction line
+xmin, xmax = plt.xlim()
+plt.plot(np.arange(xmin, xmax, 0.01), np.arange(xmin, xmax, 0.01), c='k')
+
+# Set the axis labels and show the plot
+plt.xlabel('predictions')
+plt.ylabel('actual')
+plt.legend()  # show the legend
+plt.show()
