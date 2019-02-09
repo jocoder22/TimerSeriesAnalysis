040ed40b02ff53aee3c171e749d11b939da1ede9
Okigbo <okigbookey@gmail.com>
2019-02-08 20:56:33 -0500

    Update

--- finance/finance_featureEngineering.py
+++ finance/finance_featureEngineering.py
@@ -10,15 +10,46 @@ from statsmodels.graphics.tsaplots import plot_acf
 from statsmodels.tsa.arima_model import ARMA
 from statsmodels.graphics.tsaplots import plot_pacf
 
-stocksname = ['LNG', 'SPY']
+# import talib
+
+stocksname = ['LNG']
+stocksname2 = ['SPY']
 
 startdate = datetime(2016, 4, 15)
 enddate = datetime(2018, 4, 10)
 
-stock = pdr.get_data_yahoo(stocksname, startdate, enddate)[
+lng_df = pdr.get_data_yahoo(stocksname, startdate, enddate)[
     ['Adj Close', 'Volume']]
-lng_df = stock.loc[:, (slice(None), 'LNG')]
-spy_df = stock.loc[:, (slice(None), 'SPY')]
 
-spy_df.columns = spy_df.columns.get_level_values(0)
-lng_df.columns = lng_df.columns.get_level_values('Attributes')
+spy_df = pdr.get_data_yahoo(stocksname2, startdate, enddate)[
+    ['Adj Close', 'Volume']]
+spy_df.columns = ['Adj_Close', 'Volume']
+lng_df.columns = ['Adj_Close', 'Volume']
+
+
+
+
+
+
+feature_names = ['5d_close_pct']  # a list of the feature names for later
+
+# Create moving averages and rsi for timeperiods of 14, 30, 50, and 200
+for n in [14, 30, 50, 200]:
+
+    # Create the moving average indicator and divide by Adj_Close
+    # lng_df['ma{}'.format(n)] = talib.SMA(lng_df['Adj_Close'].values,
+    #                                   timeperiod=n) / lng_df['Adj_Close']
+    # Create the RSI indicator
+    lng_df[f'rsi{n}'] = lng_df['Adj_Close'].rolling(n).mean()
+    spy_df[f'ma{n}'] = spy_df['Adj_Close'].rolling(n).mean()
+
+    # Add rsi and moving average to the feature name list
+    feature_names.extend([f'ma{n}', f'rsi{n}'])
+
+print(feature_names)
+
+print(lng_df.head())
+print(spy_df.head())
+
+print(lng_df.tail())
+print(spy_df.tail())
