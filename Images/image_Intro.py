99777b7a46d15dcf51edae855611a97758a8258d
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-02-11 20:59:39 -0500

    Update

--- Images/image_Intro.py
+++ Images/image_Intro.py
@@ -8,19 +8,25 @@ im = imageio.imread('imageio:chelsea.png')
 vv.imshow(im)
 
 
-im = imageio.imread('https://www.google.com/photos/about/static/images/hero.jpg')
-im2 = imageio.imread('https://www.google.com/photos/about/static/images/hero.jpg')
-vv.imshow(im)
-vv.imshow(im2)
+im = imageio.imread('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjpZ6-hb3L0awMVteZtZY9aEUDEK73ozF511Byna2QY7a5QJ69xw')
+im2 = imageio.imread('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUt-xUUhv0lxyBzTJP7uJj97cYZXVrgC3ebxIMP849vMGqA7z4')
+
+plt.imshow(im)
+plt.axis('off')
+plt.show()
+
+plt.imshow(im2)
+plt.axis('off')
+plt.show()
 
 print(im.shape, im.meta)
 
-vol = imageio.volread('imageio:stent.npz')
-vv.volshow(vol)
+# vol = imageio.volread('imageio:stent.npz')
+# vv.volshow(vol)
 
-for ii in range(vol.shape[0]):
-    plt.imshow(vol[:, :, ii])
-    plt.axis('off')
-    plt.pause(0.01)
+# for ii in range(vol.shape[0]):
+#     plt.imshow(vol[:, :, ii])
+#     plt.axis('off')
+#     plt.pause(0.01)
 
-print(vol.shape, vol.meta.keys())
+# print(vol.shape, vol.meta.keys())
