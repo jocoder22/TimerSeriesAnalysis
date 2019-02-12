b307c7b7422e696c2a050aa03d401148a9e22c60
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-02-11 20:19:26 -0500

    Update

--- Images/image_Intro.py
+++ Images/image_Intro.py
@@ -7,17 +7,18 @@ im = imageio.imread('imageio:chelsea.png')
 vv.imshow(im)
 
 im = imageio.imread('https://www.google.com/photos/about/static/images/hero.jpg')
+im2 = imageio.imread('https://www.google.com/photos/about/static/images/hero.jpg')
 vv.imshow(im)
-
+vv.imshow(im2)
 
 print(im.shape, im.meta)
 
 vol = imageio.volread('imageio:stent.npz')
 vv.volshow(vol)
 
-# for ii in range(vol.shape[0]):
-#     plt.imshow(vol[:, :, ii])
-#     plt.axis('off')
-#     plt.pause(0.01)
+for ii in range(vol.shape[0]):
+    plt.imshow(vol[:, :, ii])
+    plt.axis('off')
+    plt.pause(0.01)
 
 print(vol.shape, vol.meta.keys())
