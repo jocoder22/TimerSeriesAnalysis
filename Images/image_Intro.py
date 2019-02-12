23d504017f5a51a827eea9a841308340d35ed357
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-02-11 20:16:02 -0500

    Update

--- Images/image_Intro.py
+++ Images/image_Intro.py
@@ -4,19 +4,20 @@ import visvis as vv
 import matplotlib.pyplot as plt
 
 im = imageio.imread('imageio:chelsea.png')
-# vv.imshow(im)
-plt.imshow(im)
-plt.axis('off')
-plt.show()
+vv.imshow(im)
+
+im = imageio.imread('https://www.google.com/photos/about/static/images/hero.jpg')
+vv.imshow(im)
+
 
 print(im.shape, im.meta)
 
 vol = imageio.volread('imageio:stent.npz')
-# vv.volshow(vol)
+vv.volshow(vol)
 
-for ii in range(vol.shape[0]):
-    plt.imshow(vol[:, :, ii])
-    plt.axis('off')
-    plt.pause(0.01)
+# for ii in range(vol.shape[0]):
+#     plt.imshow(vol[:, :, ii])
+#     plt.axis('off')
+#     plt.pause(0.01)
 
 print(vol.shape, vol.meta.keys())
