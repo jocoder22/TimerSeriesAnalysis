d1fc786be25da4d8e7d91139178628234b2f39df
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-02-17 11:36:50 -0500

    Update

--- police_nj/analysis.py
+++ police_nj/analysis.py
@@ -5,6 +5,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 plt.style.use('ggplot')
 import gzip, requests
+from datetime import datetime
+from collections import defaultdict
 
 
 print(os.getcwd())
@@ -23,8 +25,39 @@ with open(filename, "wb") as f:
     with gzip.open(filename, 'rb') as gzfile:
         wy = pd.read_csv(gzfile)
         wy.to_csv('Data-wy.csv', index=False)
-"""
-wy = pd.read_csv('Data-wy.csv')
-print(wy.head())
 
+headers = ['col1', 'col2', 'col3', 'col4']
+dtypes = {'col1': 'str', 'col2': 'str', 'col3': 'str', 'col4': 'float'}
+parse_dates = ['col1', 'col2']
+pd.read_csv(file, sep='\t', header=None, names=headers, dtype=dtypes, parse_dates=parse_dates
+time_format = '%Y-%m-%d %H:%M'
+"""
+wy = pd.read_csv('Data-wy.csv') 
+# wy.rename({"stop_date_stop_time":"date"}, axis='columns', inplace=True)
+# wy.index.rename('Date', inplace=True)
+# wy.index = wy.index.astype('datetime64')
+
+print(wy.dtypes)
+print(wy.info())
+print(wy.head())
 print(wy.shape)
+
+
+
+ddd = dict(wy.isnull().sum())
+dd = defaultdict(list)
+nn = wy.shape[0]
+for key, value in ddd.items():
+    dd['name'].append(key)
+    dd['missing'].append(value)
+    if value == 0:
+        dd['%_missing'].append(value)
+    else:
+        val = 100 * value / nn 
+        dd['%_missing'].append(f'{val:.2f}')
+
+mm = pd.DataFrame(dd)
+print(mm)
+
+wy.drop(list(wy.columns[16:22]), axis=1, inplace=True)
+print(wy.columns)
