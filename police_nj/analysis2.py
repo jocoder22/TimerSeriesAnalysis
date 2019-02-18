02f8ede4534762213224e76bb10dd92bf2b2985e
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-02-18 14:09:54 -0500

    Update

--- police_nj/analysis2.py
+++ police_nj/analysis2.py
@@ -18,3 +18,22 @@ with open('clean_RI.pickle', 'rb') as f:
     df = pd.DataFrame(data)
 
 print(df.head())
+print(df.shape)
+
+print(df.isnull().sum())
+
+print(df.is_arrested.value_counts(normalize=True))
+print(df.dtypes)
+print(df.is_arrested.mean())  # works only for boolean, True/False
+
+print(df.groupby('district').is_arrested.mean())
+print(df.groupby(['district', 'driver_gender']).is_arrested.mean())
+print(df.groupby(['driver_gender', 'district']).is_arrested.mean())
+
+print(df.search_conducted.dtype)
+
+# Calculate the search rate by counting the values
+print(df.search_conducted.value_counts(normalize=True))
+
+# Calculate the search rate by taking the mean
+print(df.search_conducted.mean())
