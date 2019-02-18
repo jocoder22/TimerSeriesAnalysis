6c6911505592ffd37570d57bf12360526780b314
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-02-18 14:14:52 -0500

    Update

--- police_nj/analysis2.py
+++ police_nj/analysis2.py
@@ -26,14 +26,22 @@ print(df.is_arrested.value_counts(normalize=True))
 print(df.dtypes)
 print(df.is_arrested.mean())  # works only for boolean, True/False
 
+# calculate the arrest rate by district and gender
 print(df.groupby('district').is_arrested.mean())
 print(df.groupby(['district', 'driver_gender']).is_arrested.mean())
 print(df.groupby(['driver_gender', 'district']).is_arrested.mean())
 
-print(df.search_conducted.dtype)
+
 
 # Calculate the search rate by counting the values
+print(df.search_conducted.dtype)
 print(df.search_conducted.value_counts(normalize=True))
 
 # Calculate the search rate by taking the mean
 print(df.search_conducted.mean())
+
+
+# calculate the search rate by district and gender
+df['search_conducted'] = df['search_conducted'].astype('bool')
+print(df.groupby(['district', 'driver_gender']).search_conducted.mean())
+print(df.groupby(['driver_gender', 'district']).search_conducted.mean())
