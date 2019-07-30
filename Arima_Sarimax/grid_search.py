e975b0ed276969218706a9832dbed182fd43a390
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-07-30 03:18:05 -0400

    Add new file grid_search.py

---
+++ Arima_Sarimax/grid_search.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from datetime import datetime, date
+import pandas_datareader as pdr
+
+from statsmodels.tsa.stattools import adfuller
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
+from statsmodels.tsa.statespace.sarimax import SARIMAX
+from statsmodels.tsa.arima_model import ARMA
+
+sp = '\n\n'
+
+symbol = 'AAPL'
+starttime = datetime(2006, 1, 1)
+today = date.today()
+apple = pdr.get_data_yahoo(symbol, starttime, today)
+
+
+print(apple.head())
