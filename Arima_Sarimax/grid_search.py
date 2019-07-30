6341ded34ad156d1c84a175cf116f47a5b831554
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-07-30 03:32:12 -0400

    Add grid_search

--- Arima_Sarimax/grid_search.py
+++ Arima_Sarimax/grid_search.py
@@ -20,3 +20,26 @@ apple = pdr.get_data_yahoo(symbol, starttime, today)
 
 
 print(apple.head())
+
+# do grid search for parameters
+# Loop over p values from 0-3
+gridlist = []
+
+for p in range(4):
+    # Loop over q values from 0-2
+    for q in range(4):
+      
+        try:
+            # create and fit ARMA(p,q) model
+            model = SARIMAX(apple['Close'], order=(p,1,q), trend='c')
+            results = model.fit()
+            
+            # Calculate the mean absolute error from residuals
+            mae0 = np.mean(np.abs(results.resid))
+
+            # Print order and results
+            gridlist.append((p,q, results.aic, results.bic, mae0))
+            
+        except:
+            gridlist.append((p,q, None, None, None))
+
