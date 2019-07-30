96d7035859d74015ffca88e1a0a691b8e7db81f0
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-07-30 03:53:54 -0400

    Update

--- Arima_Sarimax/grid_search.py
+++ Arima_Sarimax/grid_search.py
@@ -22,30 +22,35 @@ apple = pdr.get_data_yahoo(symbol, starttime, today)
 print(apple.head())
 
 # do grid search for parameters
-# Loop over p values from 0-3
+
 gridlist = []
 
+# Loop over p values from 0-3
 for p in range(4):
+
     # Loop over q values from 0-2
     for q in range(4):
-      
-        try:
-            # create and fit ARMA(p,q) model
-            model = SARIMAX(apple['Close'], order=(p,1,q), trend='c')
-            results = model.fit()
-            
-            # Calculate the mean absolute error from residuals
-            mae0 = np.mean(np.abs(results.resid))
 
-            # Print order and results
-            gridlist.append((p,q, results.aic, results.bic, mae0))
-            
-        except:
-            gridlist.append((p,q, None, None, None))
+        # Loop over for the trend values
+        for t in ['n','c','t','ct']:
+
+            try:
+                # create and fit ARMA(p,q) model
+                model = SARIMAX(apple['Close'], order=(p,1,q), trend=t)
+                results = model.fit()
+                
+                # Calculate the mean absolute error from residuals
+                mae0 = np.mean(np.abs(results.resid))
+
+                # Print order and results
+                gridlist.append((p,q, results.aic, results.bic, mae0, t))
+                
+            except:
+                gridlist.append((p,q, None, None, None, None))
 
 # Construct DataFrame from gridlist
 griddataframe = pd.DataFrame(gridlist, 
-                        columns=['p', 'q', 'AIC', 'BIC', 'MAE'])
+                        columns=['p', 'q', 'AIC', 'BIC', 'MAE', 'T'])
 
 print(griddataframe, end=sp)
 
@@ -57,3 +62,5 @@ print('Based on BIC:\n', griddataframe.sort_values('BIC').head(1), end=sp)
 
 # Print griddataframe in order of increasing MAE
 print('Based on MAE:\n', griddataframe.sort_values('MAE').head(1), end=sp)
+
+
