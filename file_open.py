638ec5454b0011aec39ef1503be622c53797422c
Okechukwu Okigbo <okigbookey@gmail.com>
2021-03-03 14:45:46 -0500

    Update file_open.py

--- file_open.py
+++ file_open.py
@@ -1,2 +1,12 @@
 #!/usr/bin/env python
 
+import os
+
+
+with open('C:/captons/', 'rw') as f:
+  data = f.read()
+  # data2 = f.readline()
+  
+with open('filename.txt', 'r') as f:
+    last_line = f_read.readlines()[-1]
+
