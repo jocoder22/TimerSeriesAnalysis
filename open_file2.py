539805c972dc4bd6f182dd57550aaa1c8e0d7524
Okechukwu Okigbo <okigbookey@gmail.com>
2021-03-05 11:40:41 -0500

    Update open_file2.py

--- open_file2.py
+++ open_file2.py
@@ -2,3 +2,23 @@
 import os
 from pathlib import Path
 
+  
+################################# creating directory #####################################################
+# if directory or path already exists, mkdir() raises a FileExistsError:
+# using os module
+os.mkdir('example_directory/')
+
+# pathlib module
+p = Path('example_directory/')
+p.mkdir()
+
+# can use try-except to catch the error
+try:
+    p.mkdir()
+except FileExistsError as e:
+    print(e)
+
+# can ignore the FileExistsError by passing the exist_ok=True 
+p.mkdir(exist_ok=True)
+
+
