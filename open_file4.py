d9430e7bffd28fbb8e4aa52f471afb6ec7b95739
Okechukwu Okigbo <okigbookey@gmail.com>
2021-03-08 09:34:54 -0500

    Update open_file4.py

--- open_file4.py
+++ open_file4.py
@@ -14,7 +14,7 @@ from tempfile import TemporaryFile, NamedTemporaryFile, TemporaryDirectory, and 
 
 # Create a temporary file and write some data to it
 tf001 = TemporaryFile('w+t')
-tf001.write('Hello universe!')
+tf001.write('Hello Machine Learners!')
 
 # Go back to the beginning and read data from file, cos the cursor is at the end after writing to the file
 tf001.seek(0)
@@ -22,3 +22,9 @@ mydata = tf001.read()
 
 # Close the file, after which it will be removed
 tf001.close()
+
+### can use context manager for authomatic closing
+with TemporaryFile('w+t') as tf001:
+  tf001.write('Hello Machine Learners!')
+  tf001.seek(0)
+  mydata = tf001.read()
