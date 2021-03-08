a2028826410aebdb21dccd60ea14599a951a55fb
Okechukwu Okigbo <okigbookey@gmail.com>
2021-03-08 15:18:11 -0500

    Update Infor_bars.py

--- Infor_bars.py
+++ Infor_bars.py
@@ -1,1 +1,9 @@
+#!/usr/bin/env python
+import os
+from pathlib import Path
 
+def tickimbalaneT(stock):
+  stock['tick'] = stock.price.diff()
+  stock['b_t'] = stock.price.shift(periods, -1)
+  stock['b_t'] = stock.b_t
+  
