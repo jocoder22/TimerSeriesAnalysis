8604c5a6f8118992e384bca2f59b9304b18f02da
Okechukwu Okigbo <okigbookey@gmail.com>
2021-03-08 15:50:42 -0500

    Update Infor_bars.py

--- Infor_bars.py
+++ Infor_bars.py
@@ -3,7 +3,9 @@ import os
 from pathlib import Path
 
 def tickimbalaneT(stock):
-  stock['tick'] = stock.price.diff()
-  stock['b_t'] = stock.price.shift(periods, -1)
-  stock['b_t'] = stock.b_t
+  stock['PriceChange'] = stock.price.diff()
+#   stock['PriceChange_Abs'] = stock['PriceChange'].abs()/stock['PriceChange']
+  stock['PriceChange_1'] = stock.PriceChange.shift(periods, -1)
+  
+  stock['b_t'] = stock[["PriceChange_1", "PriceChange"]].apply(lambda x: x[1] if x[0] == 0 else x[1].abs()/x[1] , axis=1)
   
