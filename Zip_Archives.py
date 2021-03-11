935370de403008b3226c82cac6fd21e178710b78
Okechukwu Okigbo <okigbookey@gmail.com>
2021-03-11 11:58:36 -0500

    Update Zip_Archives.py

--- Zip_Archives.py
+++ Zip_Archives.py
@@ -1,1 +1,8 @@
+#!/usr/bin/env python
+import os
+import shutil
+import zipfile
 
+############### reading zipfile, using context manager ################################
+with zipfile.Zipfile("machineLearning.zip", "r") as zp:
+  zp.namelist() # return a list of names of files in the zip file
