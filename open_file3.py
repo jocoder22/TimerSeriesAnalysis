5122c7bc77d02e3250142fb6c93b71829c660102
jocoder22 <okigbookey@gmail.com>
2021-03-17 06:24:32 -0400

    Update

--- open_file3.py
+++ open_file3.py
@@ -85,3 +85,12 @@ for idx in range(len(symbols)):
     print2(intradata.head(), intradata.columns, n)
     print2(intradata.shape, intradata.info())
 
+
+folders = ['Apple', 'MicroSoft', 'Google', 'Netflix', 'Tesla', 'Amazon', 'Toyota', 'JPMorgan', 
+            'Citigroup', 'Walmat', 'Target', "Fedex", "Ups", "Walgreens", "Disney", "Pfizer",
+            "Cvs", "AT_T", "CocaCola", "Boeing", "SolarEdge", "AdvancedMicroDevices", "Twilio",
+            "ExpWorld", "HomeDepot", "Ford", "PVH", "Twitter", "Salesforce", 
+            "Alibaba", "NioElectricMotor", "Apple" ,  "BristolMyers"]
+symbols = ['AAPL', 'MSFT', 'GOOGL', 'NFLX', 'TSLA', 'AMZN', 'TM', 'JPM', 'C', 'WMT', 'TGT', 'FDX',
+            'UPS', 'WBA', 'DIS', 'PFE', 'CVS', 'T', 'KO', 'BA', 'SEDG', 'AMD', 'TWLO', 'EXPI', 'HD',
+              'F', 'PVH', 'TWTR', 'CRM', 'BABA', 'NIO', 'AAPL', 'BMY']
