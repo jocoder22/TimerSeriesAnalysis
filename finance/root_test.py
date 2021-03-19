c95bdf33324e686bb86b072cac009dbd9a491a14
Okechukwu Okigbo <okigbookey@gmail.com>
2021-03-19 15:02:05 -0400

    Update root_test.py

--- finance/root_test.py
+++ finance/root_test.py
@@ -1,1 +1,8 @@
+#!/usr/bin/env python
+import os
+import numpy as np
+import pandas as pd
 
+# Kwiatkowski-Phillips-Schmidt-Shin (KPSS) test (Kwiatkowski, Phillips, Schmidt, & Shin, 1992). 
+# In this test, the null hypothesis is that the data are stationary, 
+# and we look for evidence that the null hypothesis is false.
