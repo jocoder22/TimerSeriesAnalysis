5b084cdd8b400e692e4dfaa4f372f20d438244e3
Okechukwu Okigbo <okigbookey@gmail.com>
2021-03-25 13:05:00 -0400

    Update BDS_test.py

--- finance/BDS_test.py
+++ finance/BDS_test.py
@@ -17,3 +17,27 @@ print(allstocks.head())
 
 allstocks.columns = ["OpenSPY", "VIX"]
 allstocks.head()
+
+
+df = allstocks.copy()
+
+# Graphical exploration V
+fig, ax1 = plt.subplots(figsize=[14,10])
+
+color = 'tab:blue'
+ax1.set_xlabel('time (s)')
+ax1.set_ylabel('SPY Open', color=color)
+ax1.plot(df.OpenSPY, color=color)
+ax1.tick_params(axis='y', labelcolor=color)
+ax1.grid()
+ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
+
+color = 'tab:red'
+ax2.set_ylabel('CBOE VIX', color=color)  # we already handled the x-label with ax1
+ax2.plot(df.VIX, color=color)
+ax2.tick_params(axis='y', labelcolor=color)
+
+fig.tight_layout()  # otherwise the right y-label is slightly clipped
+plt.show()
+
+
