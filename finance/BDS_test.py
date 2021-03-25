e802eedce2d35439b5eaf9609cf2a606f4b6700a
Okechukwu Okigbo <okigbookey@gmail.com>
2021-03-25 14:18:21 -0400

    Update BDS_test.py

--- finance/BDS_test.py
+++ finance/BDS_test.py
@@ -9,6 +9,9 @@ from datetime import datetime
 startdate = datetime(2019, 1, 1)
 # enddate = datetime(2020, 12, 31)
 
+# statsmodels.tsa.stattools.bds(x, max_dim=2, epsilon=None, distance=1.5)[source]
+from statsmodels.tsa.stattools import bds
+
 allstocks = ["^VIX", "SPY"]
 
 # allstocks = pdr.get_data_yahoo(allstocks, startdate, enddate)['Adj Close']
@@ -40,4 +43,20 @@ ax2.tick_params(axis='y', labelcolor=color)
 fig.tight_layout()  # otherwise the right y-label is slightly clipped
 plt.show()
 
+
+def Bds_cal(x):
+    ep = [0.5, 1.0, 1.5, 2.0]
+    rangg = list(range(2,9))
+    collstat = {}
+    collpvalue = {}
+
+    for n in ep:
+        s, p = bds(df.VIX,  max_dim=8, epsilon=n)
+        collstat[n] = s.round(3)
+        collpvalue[n] = p
+        
+    stat_table = pd.DataFrame(collstat, index=rangg)
+    pvalue_table = pd.DataFrame(collpvalue, index=rangg)
+    
+    return stat_table, pvalue_table
 
