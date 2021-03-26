a7d751654ba1af56c65dd709284e1c05720185c3
Okechukwu Okigbo <okigbookey@gmail.com>
2021-03-26 12:25:07 -0400

    Update MultiVarGarch.R

--- finance/MultiVarGarch.R
+++ finance/MultiVarGarch.R
@@ -16,7 +16,15 @@ options(digits=4)
 
 # download dataset
 symbol2.vec = c("AMZN", "^GSPC")
-getSymbols(symbol2.vec, from ="2000-01-03", to = "2012-04-03")
+getSymbols(symbol2.vec, from ="2000-01-03", to = "2012-04-03", auto.assign=F)
 colnames(AMZN)
 start(AMZN)
 end(AMZN)
+
+# Merge adjusted close prices and change colnames
+alldata = merge(AMZN[, "AMZN.Adjusted", drop=F], GSPC[, "GSPC.Adjusted", drop=F])
+colnames(alldata) = c("AMZN", "GSPC")
+
+# plot adjusted close prices
+plot(alldata$AMZN, main="Amazon Adjusted Close Price")
+plot(alldata$GSPC, main="S&P500 Adjusted Close Price")
