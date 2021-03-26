dc7e657c909400a29059a5bb3bdc9deb3244ad0a
Okechukwu Okigbo <okigbookey@gmail.com>
2021-03-26 10:33:08 -0400

    Update MultiVarGarch.R

--- finance/MultiVarGarch.R
+++ finance/MultiVarGarch.R
@@ -1,1 +1,2 @@
-
+install.packages(c("PerformanceAnalytics", "quantmod", "rugarch", "car", "FinTS", "rmgarch" , "mgarch"))
+install.packages("https://cran.r-project.org/src/contrib/Archive/rmgarch/rmgarch_1.3-6.tar.gz",  repos = NULL, type="source")
