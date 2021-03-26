fb9e183a5f654f588b3c586857dad24e208f5e53
Okechukwu Okigbo <okigbookey@gmail.com>
2021-03-26 12:15:35 -0400

    Update MultiVarGarch.R

--- finance/MultiVarGarch.R
+++ finance/MultiVarGarch.R
@@ -2,3 +2,21 @@ install.packages(c("PerformanceAnalytics", "quantmod", "rugarch", "car", "FinTS"
 install.packages("https://cran.r-project.org/src/contrib/Archive/rmgarch/rmgarch_1.3-6.tar.gz",  repos = NULL, type="source")
 
 # https://www.r-graph-gallery.com/
+
+# loading required libraries
+library(PerformanceAnalytics)
+library(quantmod)
+library(rugarch)
+library(car)
+library(FinTS)
+library(rmgarch)
+library(mgarch)
+options(digits=4)
+
+
+# download dataset
+symbol2.vec = c("AMZN", "^GSPC")
+getSymbols(symbol2.vec, from ="2000-01-03", to = "2012-04-03")
+colnames(AMZN)
+start(AMZN)
+end(AMZN)
