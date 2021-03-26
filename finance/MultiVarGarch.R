ae7a7d66bfbeafc13522444b7a4d4dd8e70d871f
Okechukwu Okigbo <okigbookey@gmail.com>
2021-03-26 12:45:33 -0400

    Update MultiVarGarch.R

--- finance/MultiVarGarch.R
+++ finance/MultiVarGarch.R
@@ -37,4 +37,8 @@ plot(alldata.ret, col=c("white", "black"), main="Amazon-S&P500 Returns") # retur
 plot(alldata.ret$AMZN, main="Amazon Returns")
 plot(alldata.ret$GSPC, main="S&P500 Returns")
 
-
+# Add confidence 95% confidence interval lines
+plot(index(alldata.ret), alldata.ret$GSPC, type="l", panel.first = grid(10, lty = 1, lwd = 1), xlab="Time", ylab="S&P500 Returns")
+# lines(index(alldata.ret), tt, type = "l", col = "red", lty = 4)
+abline(h=sd(alldata.ret$GSPC)*1.96, col = "red")
+abline(h=sd(alldata.ret$GSPC)*-1.96, col = "red")
