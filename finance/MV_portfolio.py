cea324a3b681ab51972ff1349694148892295d49
Okechukwu Okigbo <okigbookey@gmail.com>
2021-03-29 09:51:39 -0400

    Update MV_portfolio.py

--- finance/MV_portfolio.py
+++ finance/MV_portfolio.py
@@ -1,9 +1,12 @@
-
+#!/usr/bin/env python
 # Load Packages
 import numpy as np
 import pandas as pd
+from datetime import datetime
 from pandas_datareader import data
 import matplotlib.pyplot as plt
 %matplotlib inline
 
+porfolio = []
+startdate = datetime(2018,1,1)
 
