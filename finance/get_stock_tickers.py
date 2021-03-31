cae8572bece8d8358b05cd4235753a0d26d47149
Okechukwu Okigbo <okigbookey@gmail.com>
2021-03-31 11:23:01 -0400

    Update get_stock_tickers.py

--- finance/get_stock_tickers.py
+++ finance/get_stock_tickers.py
@@ -5,32 +5,26 @@ from bs4 import BeautifulSoup
 import string
 
 
-def scrape_stock_symbols22(alphabet):
-    company_name2 =[]
-    company_ticker2 = []
+def get_stock_tickers(alphabet):
+    name =[]
+    ticker = []
     
     for char in alphabet: 
         URL =  f'https://www.advfn.com/nyse/newyorkstockexchange.asp?companies={char}'
         page = requests.get(URL)
         soup = BeautifulSoup(page.text, "html.parser")
-        odd_rows = soup.find_all('tr', attrs= {'class':'ts0'})
-        even_rows = soup.find_all('tr', attrs= {'class':'ts1'})
-
-        for i in odd_rows:
+        items = [i for s in ['tr.ts0', 'tr.ts1'] for i in soup.select(s)]
+    
+        for i in items:
             row = i.find_all('td')
-            company_name2.append(row[0].text.strip())
-            company_ticker2.append(row[1].text.strip())
-
-        for i in even_rows:
-            row = i.find_all('td')
-            company_name2.append(row[0].text.strip())
-            company_ticker2.append(row[1].text.strip())
+            name.append(row[0].text.strip())
+            ticker.append(row[1].text.strip())
 
         
     data = pd.DataFrame(columns = ['Company_Name',  'Company_Ticker']) 
     
-    data['Company_Name'] = company_name2
-    data['Company_Ticker'] = company_ticker2
+    data['Company_Name'] = name
+    data['Company_Ticker'] = ticker
     
     return data
 
