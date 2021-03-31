9d15e07de80af6fd97be9e659adfb20ac5fe2936
Okechukwu Okigbo <okigbookey@gmail.com>
2021-03-31 10:10:47 -0400

    Update get_stock_tickers.py

--- finance/get_stock_tickers.py
+++ finance/get_stock_tickers.py
@@ -1,1 +1,29 @@
 
+def scrape_stock_symbols22(alphabet):
+    company_name2 =[]
+    company_ticker2 = []
+    
+    for char in alphabet: 
+        URL =  f'https://www.advfn.com/nyse/newyorkstockexchange.asp?companies={char}'
+        page = requests.get(URL)
+        soup = BeautifulSoup(page.text, "html.parser")
+        odd_rows = soup.find_all('tr', attrs= {'class':'ts0'})
+        even_rows = soup.find_all('tr', attrs= {'class':'ts1'})
+
+        for i in odd_rows:
+            row = i.find_all('td')
+            company_name2.append(row[0].text.strip())
+            company_ticker2.append(row[1].text.strip())
+
+        for i in even_rows:
+            row = i.find_all('td')
+            company_name2.append(row[0].text.strip())
+            company_ticker2.append(row[1].text.strip())
+
+        
+    data = pd.DataFrame(columns = ['Company_Name',  'Company_Ticker']) 
+    
+    data['Company_Name'] = company_name2
+    data['Company_Ticker'] = company_ticker2
+    
+    return data
