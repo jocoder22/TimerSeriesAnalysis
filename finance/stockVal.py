f0835284b82e80d8bc0d5a5a700c5c4979d48748
Okechukwu Okigbo <okigbookey@gmail.com>
2021-04-08 10:46:01 -0400

    Update stockVal.py

--- finance/stockVal.py
+++ finance/stockVal.py
@@ -2,10 +2,16 @@
 import requests
 import pandas as pd
 import pandas_datareader as pdr
+import fix_yahoo_finance as yf
 import time
 import string
 
 from printdescribe import changepath
 from datetime import datetime, date
 from bs4 import BeautifulSoup
+
+asset = yf.download("GOOG", start="2000 01 01", end="2021 01 31")
+ts.adfuller(asset['Adj Close'])
+
+
 
