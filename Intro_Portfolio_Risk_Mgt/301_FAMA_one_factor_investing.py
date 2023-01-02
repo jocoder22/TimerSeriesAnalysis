c7707d5ff550d6a8444f3bed5b9edf66080bb82d
jocoder22 <okigbookey@gmail.com>
2023-01-02 18:24:30 -0500

    Update

--- Intro_Portfolio_Risk_Mgt/301_FAMA_one_factor_investing.py
+++ Intro_Portfolio_Risk_Mgt/301_FAMA_one_factor_investing.py
@@ -68,13 +68,13 @@ for col in cols:
     # Extract the beta
     CAPM_fit = CAPM_model.fit()
     regression_beta = CAPM_fit.params['Mkt_RF']
-    regressValues["Beta"].append(regression_beta)
+    regressValues["Beta"].append(round(regression_beta, 3))
 
     # Extract adjusted r-squared of the fitted regression
-    regressValues['R squared Adjusted'].append(CAPM_fit.rsquared_adj)
+    regressValues['R squared Adjusted'].append(round(CAPM_fit.rsquared_adj, 3))
 
     # Extract  r-squared of the fitted regression
-    regressValues['R squared'].append(CAPM_fit.rsquared)
+    regressValues['R squared'].append(round(CAPM_fit.rsquared, 3))
 
 regressResults = pd.DataFrame(regressValues)
 print(regressResults, **hd.sp)
