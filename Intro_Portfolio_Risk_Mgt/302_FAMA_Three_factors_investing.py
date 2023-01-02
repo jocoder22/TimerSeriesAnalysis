53e9ea2464064b2065b423b0ade4fba00fa3a788
jocoder22 <okigbookey@gmail.com>
2023-01-02 18:09:25 -0500

    Changes to be committed: 	new file:   302_FAMA_Three_factors_investing.py

---
+++ Intro_Portfolio_Risk_Mgt/302_FAMA_Three_factors_investing.py
@@ -0,0 +1,65 @@
+import os
+import numpy as np
+import pandas as pd
+from collections import defaultdict
+
+# Import statsmodels.formula.api
+import statsmodels.formula.api as smf 
+
+import mmodules.header as hd
+
+# get our Fama French portfolio data
+FF_portfolio = pd.read_pickle(os.path.join(hd.datapath, "Fama_French_PortfolioFull.pkl"))
+FF_portfolio["Mkt_RF"] = FF_portfolio["Mkt-RF"]
+FF_portfolio["SP_500_Excess"] = FF_portfolio["S&P500_Excess"]
+print(FF_portfolio.head(), FF_portfolio.tail(), FF_portfolio.columns, **hd.sp)
+
+# dropcolumns = FF_portfolio.columns[4:10]
+# data1 = FF_portfolio.drop(columns=dropcolumns)
+
+# Plot the cumulative Returns and excess returns
+title="Cumulative Returns Plot"
+ylabel = "Cumulative Portfolio Returns"
+
+# The Fama French 3-factor model:
+
+# E(rp) = RF + beta(E(rm) - RF) + beta1*SMB + beta2*HML + alpha
+
+# SMB: The small minus big factor
+# beta1: Exposure to the SMB factor
+# HML: The high minus low factor
+# beta2: Exposure to the HML factor
+# alpha: Performance which is unexplained by any other factors
+# E(rp) - RF : The excess expected return of a stock or portfolio P
+# E(rm)  : The excess expected return of the broad market portfolio B
+# RF : The regional risk free-rate
+# beta : Portfolio beta, or exposure, to the broad market portfolio B
+
+# The HML factor is constructed by calculating the return of growth stocks, or 
+# stocks with high valuations, versus the return of value stocks.
+# The SMB factor is constructed by calculating the return of small-cap stocks, or 
+# stocks with small market capitalizations, versus the return of large-cap stocks.
+
+cols = ['SP_500', 'Portfolio_MSR', 'Portfolio_GMV', 'Portfolio_GMR']
+regressValues = defaultdict(list)
+for col in cols:
+    # Define the regression formula
+    regressValues['Model'].append(col)
+    CAPM_model = smf.ols(formula=f'{col}_Excess ~ Mkt_RF', data=FF_portfolio)
+
+    # Extract the beta
+    CAPM_fit = CAPM_model.fit()
+    regression_beta = CAPM_fit.params['Mkt_RF']
+    regressValues["Beta"].append(regression_beta)
+
+    # Print adjusted r-squared of the fitted regression
+    regressValues['R squared Adjusted'].append(CAPM_fit.rsquared_adj)
+
+    # Print  r-squared of the fitted regression
+    regressValues['R squared'].append(CAPM_fit.rsquared)
+
+regressResults = pd.DataFrame(regressValues)
+print(regressResults, **hd.sp)
+
+
+
