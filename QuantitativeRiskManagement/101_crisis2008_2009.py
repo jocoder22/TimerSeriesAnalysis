eaa2ba34fa8ebd3ea955978676564754b213b988
jocoder22 <okigbookey@gmail.com>
2023-01-05 22:28:44 -0500

    Changes to be committed: 	modified:   101_crisis2008_2009.py

--- QuantitativeRiskManagement/101_crisis2008_2009.py
+++ QuantitativeRiskManagement/101_crisis2008_2009.py
@@ -20,9 +20,34 @@ n = len(portfolio.columns)
 weights = np.repeat(1/n, n)
 
 # Compute the portfolio's daily returns
-asset_returns = asset_prices.pct_change()
+asset_returns = asset_prices.pct_change().dropna()
 portfolio_returns = asset_returns.dot(weights)
 
 # Plot portfolio returns
 portfolio_returns.plot().set_ylabel("Daily Return, %")
+plt.show()
+
+
+# Generate the covariance matrix from portfolio asset's returns
+covariance = asset_returns.cov()
+
+# Annualize the covariance using 252 trading days per year
+covariance = covariance * 252
+
+# Display the covariance matrix
+print(covariance)
+
+# Compute and display portfolio volatility for 2008 - 2009
+portfolio_variance = np.transpose(weights) @ covariance @ weights
+portfolio_volatility = np.sqrt(portfolio_variance)
+print(portfolio_volatility)
+
+# Calculate the 30-day rolling window of portfolio returns
+returns_windowed = portfolio_returns.rolling(30)
+
+# Compute the annualized volatility series
+volatility_series = returns_windowed.std()*np.sqrt(252)
+
+# Plot the portfolio volatility
+volatility_series.plot().set_ylabel("Annualized Volatility, 30-day Window")
 plt.show()
--- QuantitativeRiskManagement/HelperFunctions/getData.py
+++ QuantitativeRiskManagement/HelperFunctions/getData.py
@@ -2,6 +2,9 @@
 import pandas as pd
 import yfinance as yf
 
+import pandas_datareader.data as web
+import datetime
+
 from printdescribe import changepath
 
 datapath = r"E:\TimerSeriesAnalysis\QuantitativeRiskManagement\Data"
@@ -14,10 +17,23 @@ startdate = "2005-01-01"
 enddate = "2010-12-31"
 
 print("[getData] Downloading data ...")
+
 stockdata = yf.download(tickers, start=startdate, end=enddate)[['Adj Close']].droplevel(0, axis=1)
 stockdata.columns = Assets
 
+mort_del_rate_fred = web.DataReader('DRSFRMACBS', 'fred', startdate, enddate)
+mort_30rate_fred = web.DataReader('MORTGAGE30US', 'fred', startdate, enddate)
+mort__Income_fred = web.DataReader('MDSP', 'fred', startdate, enddate)
+
+weblink = "https://s3.amazonaws.com/files.consumerfinance.gov/data/mortgage-performance/downloads/CountyMortgagesPercent-90-plusDaysLate-thru-2022-06.csv"
+
+mort_del_rate_cusFin = pd.read_csv(weblink)
+
 with changepath(datapath):
     stockdata.to_csv(f"assetsData.csv")
+    mort_del_rate_cusFin.to_csv('mort_del_R_cf.csv')
+    mort_del_rate_fred.to_csv('mort_del_R_fred.csv')
+    mort_30rate_fred.to_csv('mort_30rate_R_fred.csv')
+    mort__Income_fred.to_csv('mort_income_fred.csv')
 
 print("[getData] Done Downloading data!")
